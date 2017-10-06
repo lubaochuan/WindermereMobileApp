7c0d05ca9ce8d6dd4a1eb92e3387fee7de6a30c4
DerekSweiger <31293837+DerekSweiger@users.noreply.github.com>
2017-10-06 09:39:45 -0500

    Changing buttons on home Screen to TouchableHighlights

--- HomeScreen.js
+++ HomeScreen.js
@@ -6,6 +6,7 @@ import {
   Text,
   StyleSheet,
   Button,
+  TouchableHighlight
 } from 'react-native';
 
 
@@ -18,16 +19,21 @@ export class HomeScreen extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Button
-          title='Go to Profile'
-          onPress= {() => navigate('Profile')
-          }
-        />
-        <Button
-          title='Go to Page 3'
-          onPress={() => navigate('Page3')
-          }
-        />
+        <TouchableHighlight
+          onPress= {() => navigate('Profile')}>
+
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Go to Profile</Text>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          onPress= {() => navigate('Page3')}>
+          
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Go to Page 3</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -37,4 +43,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    alignItems: 'center',
+    margin: 10,
+    padding: 15,
+    backgroundColor: 'grey',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  }
 });
