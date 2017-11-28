9915ca19bd895faf4d3757c36fb7b803ee7ae4a4
DerekSweiger <31293837+DerekSweiger@users.noreply.github.com>
2017-11-27 21:28:21 -0600

    Adding map to map tab

--- Pages/MapScreen.js
+++ Pages/MapScreen.js
@@ -4,16 +4,24 @@ import {
   Text,
   StyleSheet,
   Button,
-  TouchableHighlight
+  TouchableHighlight,
+  Image,
+  Dimensions
 } from 'react-native';
 
-
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 export class MapScreen extends React.Component {
   render() {
+
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Map Page</Text>
+        <Image
+          style={styles.mapImage}
+          source={require('../assets/map.jpg')}
+          resizeMode="stretch"
+        />
       </View>
     );
   }
@@ -23,10 +31,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+
   },
   text: {
     fontSize: 30,
     color: 'grey'
+  },
+  mapImage: {
+    flex: 1,
+    height: screenHeight,
+    width: screenWidth
+
   }
 });
