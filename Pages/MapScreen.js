c473be7546cc632fb0f282c5e211e371dbe167cc
DerekSweiger <31293837+DerekSweiger@users.noreply.github.com>
2018-02-15 12:41:05 -0600

    Replacing static map with MapView

--- Pages/MapScreen.js
+++ Pages/MapScreen.js
@@ -8,16 +8,17 @@ import {
   Image,
   Dimensions
 } from 'react-native';
+import { MapView } from 'expo';
 
 //Will contain the interactive map, eventually.
 //Currently contains a static placeholder image of the windermere campus.
 
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+//const screenWidth = Dimensions.get('window').width;
+//const screenHeight = Dimensions.get('window').height;
 
 export class MapScreen extends React.Component {
+/*
   render() {
-
     return (
       <View style={styles.container}>
         <Image
@@ -27,6 +28,22 @@ export class MapScreen extends React.Component {
       </View>
     );
   }
+*/
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: 38.0326914,
+          longitude: -92.8380096,
+          latitudeDelta: 0.0080,
+          longitudeDelta: 0.0080,
+        }}
+        mapType="hybrid"
+      />
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -43,8 +60,8 @@ const styles = StyleSheet.create({
   },
   mapImage: {
     flex: 1,
-    height: screenHeight,
-    width: screenWidth,
+    //height: screenHeight,
+    //width: screenWidth,
     resizeMode: 'stretch',
   }
 });
